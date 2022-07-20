d84ae332783e868054ff118fe3fd24626b7c15dd
Michael Manlulu <michael.manlulu@myresman.com>
2022-07-21 02:18:18 +0800

    410. adding a data model

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css';
+import Todo from './models/todo';
 import Todos from './components/Todos';
 
 function App() {
+  const todos = [new Todo('Learn React'), new Todo('Learn React')];
+
   return (
     <div>
-      <Todos items={['Learn React', 'Learn TypeScript']} />
+      <Todos items={todos} />
     </div>
   );
 }
