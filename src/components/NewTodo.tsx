0cb08509d3a2d5afe3b2b20c1e0bc4b5042be85b
Michael Manlulu <michael.manlulu@myresman.com>
2022-07-22 14:50:40 +0800

    413. working with refs & useRef

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,12 +1,21 @@
+import { useRef } from 'react';
+
 const NewTodo = () => {
+  const textInputRef = useRef<HTMLInputElement>(null);
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
+
+    const enteredText = textInputRef.current!.value;
+
+    if (enteredText.trim().length === 0) {
+      return;
+    }
   };
 
   return (
     <form onSubmit={submitHandler}>
       <label htmlFor='text'>Todo text</label>
-      <input type='text' name='text' id='text' />
+      <input type='text' name='text' id='text' ref={textInputRef} />
       <button type='submit'>Add Todo</button>
     </form>
   );
