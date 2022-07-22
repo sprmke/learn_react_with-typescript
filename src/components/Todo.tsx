f62085b05dc7b726affdeae29ebf9e4933dd948e
Michael Manlulu <michael.manlulu@myresman.com>
2022-07-22 16:34:36 +0800

    417. Optional to add React.MouseEvent

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -4,7 +4,7 @@ import classes from './Todo.module.css';
 
 const Todo: React.FC<{
   text: string;
-  onRemoveTodo: () => void;
+  onRemoveTodo: (event: React.MouseEvent) => void;
 }> = (props) => {
   return (
     <li className={classes.item} onClick={props.onRemoveTodo}>
