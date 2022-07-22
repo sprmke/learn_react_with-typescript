bd1f5d001f84639e252d00d82e5632a0d827eb5b
Michael Manlulu <michael.manlulu@myresman.com>
2022-07-22 15:16:07 +0800

    415. managing state & typescript

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import './App.css';
 import TodoModel from './models/todo-model';
 import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
+import { useState } from 'react';
 
 function App() {
-  const todos = [new TodoModel('Learn React'), new TodoModel('Learn React')];
+  const [todos, setTodos] = useState<TodoModel[]>([]);
 
-  const addTodoHandler = (todoText: string) => {};
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new TodoModel(todoText);
+
+    setTodos((currTodos) => currTodos.concat(newTodo));
+  };
 
   return (
     <div>
